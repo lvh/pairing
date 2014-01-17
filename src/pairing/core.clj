c610c984092cf8e3cb26a48fc1607fde5cf4246b
Laurens Van Houtven <_@lvh.cc>
2014-01-17 23:29:58 +0100

    Add pair-with-matching-prefs

--- src/pairing/core.clj
+++ src/pairing/core.clj
@@ -1,12 +1,31 @@
 (ns pairing.core
   [:require [clj-time.core :refer (local-date)]])
 
-(defn prefs
+(def prefs
   "Gets the grouping preferences for a user (gender, start-date, end-date)."
-  [x]
-  ((juxt :gender :start-date :end-date) x))
+  (juxt :gender :start-date :end-date))
 
 (defn group-by-prefs
   "Groups the given pool of users by gender, start-date, end-date."
   [pool]
   (group-by prefs pool))
+
+(defn pairs-and-leftovers
+  "Gets [pairs, leftovers] in subgroup."
+  [subgroup]
+  (let [[pairs leftovers] (split-with #(= (count %) 2) (partition-all 2 subgroup))]
+    [pairs (apply concat leftovers)]))
+
+(defn pair-with-matching-prefs
+  "Pairs people from pool with matching prefs. Returns pairs and
+  remaining ungrouped people."
+  [pool]
+  (loop [all-pairs []
+         all-leftovers []
+         subgroups (vals (group-by-prefs pool))]
+    (if (seq subgroups)
+      (let [[new-pairs new-leftovers] (pairs-and-leftovers (first subgroups))]
+        (recur (into all-pairs new-pairs)
+               (into all-leftovers new-leftovers)
+               (next subgroups)))
+      [all-pairs all-leftovers])))
--- test/pairing/core_test.clj
+++ test/pairing/core_test.clj
@@ -16,13 +16,21 @@
             :gender :female
             :start-date (local-date 2014 01 10)
             :end-date (local-date 2014 01 20)})
+(def dave {:name "Dave"
+           :gender :male
+           :start-date (local-date 2014 01 10)
+           :end-date (local-date 2014 01 15)})
+(def mallory {:name "Mallory"
+              :gender :male
+              :start-date (local-date 666 6 6)
+              :end-date (local-date 666 6 10)})
 
-(deftest prefs-test
+(deftest prefs-tests
   (testing "get preferences from a user"
     (is (= (prefs alice)
            [:female (:start-date alice) (:end-date alice)]))))
 
-(deftest group-by-prefs-test
+(deftest group-by-prefs-tests
   (testing "empty pool"
     (is (= (group-by-prefs []) {})))
   (testing "one person"
@@ -39,3 +47,38 @@
     (is (= (group-by-prefs [alice bob carol])
            {(prefs alice) [alice carol]
             (prefs bob) [bob]}))))
+
+(deftest pairs-and-leftovers-tests
+  (testing "empty -> no pairs, no leftovers"
+    (is (= (pairs-and-leftovers [])
+           [[]
+            []])))
+  (testing "one person -> no pairs, one leftover"
+    (is (= (pairs-and-leftovers [alice])
+           [[]
+            [alice]])))
+  (testing "two persons -> one pair, no leftovers"
+    (is (= (pairs-and-leftovers [alice bob])
+           [[[alice bob]]
+            []])))
+  (testing "three persons -> one pair, one leftover"
+    (is (= (pairs-and-leftovers [alice bob carol])
+           [[[alice bob]]
+            [carol]]))))
+
+(deftest pair-with-matching-prefs-tests
+  (testing "empty"
+    (is (= (pair-with-matching-prefs [])
+           [[] []])))
+  (testing "one pair, one extra"
+    (is (= (pair-with-matching-prefs [alice bob carol])
+           [[[alice carol]]
+            [bob]])))
+  (testing "two pairs"
+    (is (= (pair-with-matching-prefs [alice bob carol dave])
+           [[[alice carol] [bob dave]]
+            []])))
+  (testing "two pairs, one extra"
+    (is (= (pair-with-matching-prefs [alice bob carol dave mallory])
+           [[[alice carol] [bob dave]]
+            [mallory]]))))
